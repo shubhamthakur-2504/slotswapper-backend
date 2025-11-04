9fa236c628bde247d19c80b9446b4d460b04deab
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-11-05 02:24:22 +0530

    chore: updated const and env sample

--- const.js
+++ const.js
@@ -7,4 +7,10 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const MONGO_URL = process.env.MONGO_URL;
 const db_name = "slotswapper_db";
 
-export { PORT, CLIENT_URL, MONGO_URL, db_name };
+// jwt
+const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET
+const JWT_ACCESS_TOKEN_EXPIRY = Number(process.env.JWT_ACCESS_TOKEN_EXPIRY)
+const JWT_REFRESH_TOKEN_EXPIRY = Number(process.env.JWT_REFRESH_TOKEN_EXPIRY)
+
+export { PORT, CLIENT_URL, MONGO_URL, db_name, JWT_ACCESS_SECRET, JWT_REFRESH_SECRET, JWT_ACCESS_TOKEN_EXPIRY, JWT_REFRESH_TOKEN_EXPIRY };
