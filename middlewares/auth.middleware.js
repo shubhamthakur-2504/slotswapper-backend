cc0a2b2b1f589a88341fe04a968e9cd55d89184b
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-11-05 23:16:01 +0530

    feat: added auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import JWT from "jsonwebtoken";
+import apiError from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import { JWT_ACCESS_SECRET } from "../const.js";
+
+const verifyJwtToken = asyncHandler(async (req, _, next) => {
+    const token = req.headers.authorization?.split(" ")[1] || req.cookies.accessToken
+
+    if (!token) {
+        throw new apiError(401, "Access token is required")
+    }
+    try {
+        const decodedToken = JWT.verify(token, JWT_ACCESS_SECRET);
+        const user = await User.findById(decodedToken?.id).select("-password -refreshToken")
+
+        if (!user) {
+            throw new apiError(401, "Invalid access token")
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        throw new apiError(401, error?.message || "Invalid access token")
+    }
+})
+
+export default verifyJwtToken
