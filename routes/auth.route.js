9f909a298573fd6da5ccde294ec9475f59e387f4
shubhamthakur-2504 <shubhamthakur60278@gmail.com>
2025-11-07 00:19:05 +0530

    feat: auth route added

---
+++ routes/auth.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { register, login, refreshAccessToken, logout, getUser } from "../controllers/auth.controller.js";
+import {verifyJwtToken as auth} from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/register").post(register)
+router.route("/login").post(login)
+router.route("/refresh-access-token").post(auth, refreshAccessToken)
+router.route("/logout").post(auth, logout)
+router.route("/get-user").get(auth, getUser)
+
+export default router
